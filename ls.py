deb346eab6cfdc3eec3ab0606f6a9fdd196cb495
lv <aaa@qq.com>
2017-01-29 17:35:34 +0800

    abc

--- ls.py
+++ ls.py
@@ -1,8 +1,11 @@
-import argparse,os
+import argparse, os, stat
+import datetime
+from functools import partial
 
 
 def opt():
-    opt = argparse.ArgumentParser(prog='list program', description='writing like the linux list command')
+    opt = argparse.ArgumentParser(prog='list program', add_help=False,
+                                  description='writing like the linux list command')
     opt.add_argument('argvs',
                      type=str,
                      nargs='*')
@@ -16,40 +19,108 @@ def opt():
                      default=False,
                      dest='a',
                      help='list all file')
+    opt.add_argument('-h',
+                     action='store_true',
+                     default=False,
+                     dest='h',
+                     help='count size')
     return opt.parse_args()
 
 
+def lsmore(x):
+    uid = {}
+    gid = {}
+    with open('/etc/passwd') as f:
+        ne = f.readline()
+        while ne:
+            tu = ne.split(':')
+            uid[tu[2]] = tu[0]
+            ne = f.readline()
+    with open('/etc/group') as g:
+        gn = g.readline()
+        while gn:
+            gt = gn.split(':')
+            gid[gt[2]] = gt[0]
+            gn = g.readline()
+    lst = [str(i) for i in range(9)]
+    st = os.stat(x)
+    time = datetime.datetime.fromtimestamp(st.st_ctime)
+    lst[0] = stat.filemode(st.st_mode)
+    if os.path.isdir(x):
+        lst[1] = str(len(os.listdir(x)))
+    lst[2] = uid[str(st.st_uid)]
+    lst[3] = gid[str(st.st_gid)]
+    lst[4] = str(st.st_size)
+    lst[5] = time.strftime('%b')
+    lst[6] = time.strftime('%d')
+    lst[7] = time.strftime('%H:%M')
+    lst[8] = os.path.basename(x)
+    return ' '.join(lst)
+
+
+def alllist(x):
+    f = ['.', '..']
+    if x == '.':
+        f.extend(os.listdir('.'))
+        for st in range(6, len(f) + 1, 6):
+            print(' '.join(f[st - 6:st]))
+    elif os.path.isdir(x):
+        f.extend(os.listdir(x))
+        for st in range(6, len(f) + 1, 6):
+            print(f[st - 6:st])
+    else:
+        print(x)
+
+
+def option_stat(opt, y):
+    if not opt.a and not opt.h and not opt.l:
+        if os.path.isfile(y):
+            print(y)
+        else:
+            for j in os.listdir(y):
+                if j[0] != '.':
+                    print(j, end='  ')
+            print()
+    elif opt.a:
+        if os.path.isfile(y):
+            print(y)
+        else:
+            alllist(y)
+    elif opt.l:
+        if os.path.isfile(y):
+            print(lsmore(y))
+        else:
+            total=0
+            for l in os.listdir(y):
+                print(l)
+                total += os.stat(y+'/'+l).st_size
+            print('total: %s'% total)
+            for i in os.listdir(y):
+                print(lsmore(i))
+    elif opt.l and opt.h:
+        pass
+    elif opt.a and opt.h:
+        if os.path.isfile(y):
+            print(y)
+        else:
+            alllist(y)
+
+
+def main():
+    op = opt()
+    st = partial(option_stat, op)
+    if not op.argvs:
+        st('.')
+    else:
+        if len(op.argvs) == 1:
+            if op.argvs[0] == '.':
+                st('.')
+            else:
+                st(op.argvs[0])
+        else:
+            for k in op.argvs:
+                st(k)
+
 
 if __name__ == '__main__':
-    op = opt()
-
-    if len(op.argvs) == 1:
-        if op.argvs[0] == '.':
-            if op.a:
-                print('.')
-                print('..')
-                print('   '.join(os.listdir(op.argvs.pop())))
-            else:
-                for p in os.listdir(op.argvs.pop()):
-                    if p[0] != '.':
-                        print(p,end='   ')
-                print()
-        elif os.path.isfile(op.argvs[0]):
-            print(op.argvs.pop())
-        else:
-            if op.a:
-                print('.')
-                print('..')
-                print('   '.join(os.listdir(op.argvs.pop())))
-            else:
-                for k in os.listdir(op.argvs.pop()):
-                    if k[0] != '.':
-                        print(k,end='  ')
-    else:
-        for i in op.argvs:
-            if os.path.isfile(i):
-                print(i,end='  ')
-            else:
-                print('\n')
-                print('%s:'%i)
-                print('   '.join(os.listdir(i)))
+    main()
--- nn/clockdate.py
+++ nn/clockdate.py
@@ -1,22 +1,34 @@
 import threading, sys
 import datetime, socket
-from pytz import timezone
+from pytz import timezone, all_timezones
 import argparse
 
 
 def opt():
     options = argparse.ArgumentParser(description='out put time size')
+    options.add_argument('address',
+                         nargs='?',
+                         type=str,
+                         help='bind address'
+                         )
+    options.add_argument('-s',
+                         '--show-timezone',
+                         action='store_true',
+                         default=False,
+                         dest='show',
+                         help='show how many timezone you can use it'
+                         )
     options.add_argument(
         '--port', '-p',
-        nargs='*',
+        nargs='?',
         dest='port',
-        help='bind in port'
+        help='bind in port,you can use "," to more ports'
     )
     options.add_argument(
         '--timezone', '-z',
-        nargs='*',
+        nargs='?',
         dest='zone',
-        help='select timezone to show'
+        help='select timezone to show, you can use "," to more timezone'
     )
     return options.parse_args()
 
@@ -28,46 +40,49 @@ def Tzone(zones='Asia/Shanghai'):
         return now
 
 
-def sock_bind(ip, port, n,e):
+def sock_bind(ip, port, time):
+    event = threading.Event()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.bind((ip, int(port)))
-        s.listen()
+    s.bind((ip, int(port)))
+    s.listen()
+
+    def client_send(con, zone, ev):
+        o = opt()
+        try:
+            while not ev.is_set():
+                if o.zone and zone <= len(o.zone.split(','))-1:
+                    ret = Tzone(o.zone.split(',')[zone])
+                else:
+                    ret = Tzone()
+                con.send(ret.encode() + b'\n')
+                ev.wait(1)
+        except BrokenPipeError:
+            return con.close()
+        return
+
+    while not event.is_set():
         (conn, client) = s.accept()
-        while True:
-            if op.zone:
-                ret = Tzone(op.zone[n])
-            else:
-                ret = Tzone()
-            conn.send(ret.encode() + b'\n')
-            e.wait(1)
-    except BrokenPipeError:
-        pass
-    except KeyboardInterrupt:
-        pass
-    except StopIteration:
-        print('is ending...')
-        e.set()
-    conn.close()
+        t = threading.Thread(target=client_send, args=(conn, time, event))
+        t.start()
+        event.wait(1)
+    return
 
 
 if __name__ == '__main__':
-    event = threading.Event()
-    threadinglist = []
     op = opt()
-    if not op.port:
+    if op.show:
+        for x in all_timezones:
+            print(x)
         sys.exit()
 
-    for n, p in enumerate(op.port[1:]):
-            threadinglist.append(threading.Thread(target=sock_bind, args=(op.port[0], p, n,event)))
-    print(threadinglist)
-    while True:
-        try:
+    if not op.address:
+        print('not bind any IP address,please retry.')
+        sys.exit()
 
-            for tread in threadinglist:
-                if not tread.is_alive():
-                    print(threadinglist)
-                    tread.start()
+    if not op.port:
+        print('not bind any ports,please retry.')
+        sys.exit()
 
-        except KeyboardInterrupt:
-            pass
+    for n, p in enumerate(op.port.split(',')):
+        b = threading.Thread(target=sock_bind, args=(op.address, p, n))
+        b.start()
