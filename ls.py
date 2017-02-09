e9d7e7b23744b2b48d92c0cc61f3f894c407a89a
lv <lv@lives.com>
2017-02-09 21:59:22 +0800

    abc

--- ls.py
+++ ls.py
@@ -26,6 +26,16 @@ def opt():
     return opt.parse_args()
 
 
+def color(x):
+    st = os.stat(x, follow_symlinks=False)
+    if stat.S_ISLNK(st.st_mode):
+        return '\033[1;36m{}\033[0m'.format(x)
+    if os.path.isdir(x):
+        return '\033[1;34m{}\033[0m'.format(x)
+    else:
+        return '{}'.format(x)
+
+
 def more(x, h=False):
     uid = {}
     gid = {}
@@ -42,17 +52,17 @@ def more(x, h=False):
             gid[gt[2]] = gt[0]
             gn = g.readline()
     lst = [str(i) for i in range(9)]
-    st = os.stat(x)
+    st = os.stat(x, follow_symlinks=False)
     time = datetime.datetime.fromtimestamp(st.st_ctime)
     lst[0] = stat.filemode(st.st_mode)
-    lst[1] = '{:>2}'.format(str(len([v for v in os.listdir(x) if v[0] != '.']) if os.path.isdir(x) else 1))
+    lst[1] = '{:>2}'.format(st.st_nlink)
     lst[2] = uid[str(st.st_uid)]
     lst[3] = gid[str(st.st_gid)]
     if h:
         for n, i in enumerate([(1024 ** 3, 'G'), (1024 ** 2, 'M'), (1024, 'K'), (0, '')]):
             if st.st_size > i[0]:
                 size = st.st_size / 1024 ** (3 - n)
-                if size > 100 and i[0] != 0:
+                if size > 100 and i[0] != 0 and os.path.isfile(x):
                     lst[4] = '{:>4.4}{}'.format('%d' % (size), i[1])
                 elif i[0] == 0:
                     lst[4] = '{:>5}'.format(str(int(st.st_size)))
@@ -60,44 +70,47 @@ def more(x, h=False):
                     lst[4] = '{:>4.4}{}'.format('%0.2f' % (size), i[1])
                 break
     else:
-        lst[4] = '{:>6}'.format(str(st.st_size))
+        lst[4] = '{:>5}'.format(str(st.st_size))
     lst[5] = time.strftime('%b')
     lst[6] = time.strftime('%d')
     lst[7] = time.strftime('%H:%M')
-    lst[8] = os.path.basename(x)
+    lst[8] = '{} -> {}'.format(color(os.path.basename(x)), os.readlink(x)) if os.path.islink(x) else color(os.path.basename(x))
     return ' '.join(lst)
 
 
 def show_all(x):
     if os.path.isfile(x):
-        print(x)
+        print(color(x))
     f = ['.', '..']
     if x == '.':
         f.extend(os.listdir('.'))
-        for st in range(6, len(f) + 1, 6):
-            print(' '.join(f[st - 6:st]))
+        pf=sorted([color(x) for x in f])
+        for st in range(6, len(pf) + 1, 6):
+            print('  '.join(pf[st - 6:st]))
     elif os.path.isdir(x):
         f.extend(os.listdir(x))
-        for st in range(6, len(f) + 1, 6):
-            print(f[st - 6:st])
+        pf = sorted([color(x) for x in f])
+        for st in range(6, len(pf) + 1, 6):
+            print('  '.join(pf[st - 6:st]))
     else:
-        print(x)
+        print(color(x))
 
 
 def Show(x):
     if os.path.isfile(x):
-        print(x)
+        print(color(x))
     else:
         for i in os.listdir(x):
             if i[0] != '.':
-                print(i, end='  ')
+                print(color(i), end='  ')
         print()
 
 
 def _more(x):
     o = opt()
-    if os.path.isfile(x):
+    if os.path.isfile(x) or os.path.islink(x):
         print(more(x))
+        return
 
     if o.h and o.a:
         total = 0
@@ -107,6 +120,8 @@ def _more(x):
             if total > p[0]:
                 print('total: %s%s' % (int(total // 1024 ** (3 - n)), p[1]))
                 break
+        print(more('.'))
+        print(more('..'))
         for i in os.listdir(x):
             print(more(x + '/' + i, h=True))
     elif o.h:
@@ -149,10 +164,10 @@ def option_stat(dest):
         show_all(dest)
     elif op.h:
         if os.path.isfile(dest):
-            print(dest)
+            print(color(dest))
         else:
             for k in os.listdir(dest):
-                print(k,end='  ')
+                print(color(k), end='  ')
             print()
 
 
--- url.py
+++ url.py
@@ -1,12 +1,10 @@
 from urllib import request
 from urllib import parse
 
-url = 'http://121.10.141.71:8081/chart'
-head = {
-    'Content-Type':'application/x-www-form-urlencoded'
-}
+url = 'http://121.10.141.71:8081/api/counters'
+head = {'Content-Type':'application/x-www-form-urlencoded'}
 
-data = {'endpoints[]':'113.107.161.24','counters[]':'df.bytes.total/stype=NTFS','graph_type':'h'}
+data = {'endpoints':'113.107.161.24'}
 data = parse.urlencode(data).encode('ascii')
 req = request.Request(url,data)
 page = request.urlopen(req).read()
