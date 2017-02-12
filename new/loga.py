a4c65f4f6ceebbfbea0e360669eaf59ef3535d8f
lv <lv@lives.com>
2017-02-12 16:34:29 +0800

    abc

--- new/loga.py
+++
@@ -1,9 +0,0 @@
-import re
-import datetime
-import threading
-from collections import namedtuple
-
-
-matcher = r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) .* \[(?P<time>.*)\] "(?P<method>\w+) (?P<url>[^\s]*) (?P<version>[\w|/\.\d]*)" (?P<status>\d{3}) (?P<length>\d+) "(?P<referer>[^\s]*)" "(?P<ua>.*)"'
-
-
