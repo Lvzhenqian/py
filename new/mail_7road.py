a325bef0cabe69091d07af9ab50f351789c1cb5a
lv <lv@lives.com>
2017-02-18 20:08:35 +0800

    abc

--- new/mail_7road.py
+++ new/mail_7road.py
@@ -10,26 +10,20 @@ lock = threading.RLock()
 
 
 def conn(x, n):
-    try:
-        lock.acquire()
-        global mail
-        if b'FTP-IN.7ROAD.COM' in x.top(n, 0)[1][0]:
-            mail.append(n)
-            return
-        else:
-            return
-    finally:
-        lock.release()
+    global mail
+    if b'FTP-IN.7ROAD.COM' in x.top(n, 0)[1][0]:
+        mail.append(n)
+        return
+    return
 
 
-def read_body(c, x, code=None):
+def read_body(c, x):
+    c.noop()
     tmp = []
     flag = False
-    o = re.compile(
-        r'OA单号为:(?P<oa>.*)<br/>成.*md5:(?P<md5>.*)<br/>作.*project:::(?P<project>.*)dba:::(?P<dba>\w{1,3})cross:::(?P<cross>\w{1,3})yufabu:::(?P<yufabu>\w{1,3})stop:::(?P<stop>\w{1,3}).*<a href=\'(?P<url>.*)\'>点.*')
+    o = re.compile(r'OA.*:(?P<OA>\d+)<.*md5:(?P<md5>\w+).*<a href=\'(?P<url>.*)\'.*')
     for i in c.retr(x)[1]:
         if i.startswith(b'Content-Transfer-Encoding'):
-            code = i.rstrip()[-1]
             flag = True
             continue
         elif i.endswith(b'==--'):
@@ -37,10 +31,9 @@ def read_body(c, x, code=None):
         elif flag:
             tmp.append(i)
     body = b''.join(tmp)
-    if code == 'base64':
-        ret = o.match(body_decode(body).decode('utf-8'))
-        if ret:
-            return ret.groupdict()
+    ret = o.match(body_decode(body).decode('utf-8'))
+    if ret:
+        return ret.groupdict()
 
 
 if __name__ == '__main__':
@@ -52,15 +45,16 @@ if __name__ == '__main__':
     con.pass_('angelo_5566!@')
     total_mail = con.stat()[0]
     print('mail server have: {}'.format(total_mail))
-    for i in range(total_mail, int(total_mail // 2), -1):
+    for i in range(total_mail, 1, -1):
         count.append(threading.Thread(target=conn, args=(con, i)))
     for t in count:
-        if not mail:
-            t.start()
-            t.join(1)
+        t.start()
+        t.join(1)
+    con.noop()
     for em in mail:
-        dic = read_body(con,em)
+        dic = read_body(con, em)
         print(dic)
     end = datetime.now() - now
     print(end.total_seconds())
-
+    con.quit()
+    con.close()
