947722207779c6122f068d3a73233319b3a36e34
Lv.zhenqian <276829290@qq.com>
2017-05-02 16:39:29 +0800

    abc

--- ddt/change.py
+++ ddt/change.py
@@ -13,6 +13,14 @@ class ET(cElementTree.TreeBuilder):
 
 class change:
 	def __init__(self):
+		# self.__Center_File = r'D:\dandantang\Center\Center.Service.exe.config'
+		# self.__key = r'D:\dandantang\Center\key.txt'
+		# self.__Road_config = r'D:\dandantang\Server1\Road.Service.exe.config'
+		# self.__Flash_config = r'D:\dandantang\Flash\config.xml'
+		# self.__Flash_web = r'D:\dandantang\Flash\web.config'
+		# self.__Flash_Default = r'D:\dandantang\Flash\Default.aspx'
+		# self.__request_config = r'D:\dandantang\Request\web.config'
+		# self.__host = r'C:\Windows\System32\drivers\etc\hosts'
 		self.__Center_File = r'D:\dandantang\Center\Center.Service.exe.config'
 		self.__key = r'D:\dandantang\Center\key.txt'
 		self.__Road_config = r'D:\dandantang\Server1\Road.Service.exe.config'
@@ -21,6 +29,7 @@ class change:
 		self.__Flash_Default = r'D:\dandantang\Flash\Default.aspx'
 		self.__request_config = r'D:\dandantang\Request\web.config'
 		self.__host = r'C:\Windows\System32\drivers\etc\hosts'
+		self.__dst = lambda st,dt: st.replace(urlparse(st).netloc, dt)
 
 	def make_tree(self, file: str) -> cElementTree.parse:
 		backup = '_'.join((file, time.strftime('%Y-%m-%d')))
@@ -52,17 +61,17 @@ class change:
 		tree.write(self.__Road_config, encoding='utf-8', xml_declaration=True)
 
 	def Flash_config(self, Req, parter_id, assist):
-		dst = lambda st: st.replace(urlparse(st).netloc, assist)
 		tree = self.make_tree(self.__Flash_config)
 		root = tree.getroot()
-		root.find('config/REQUEST_PATH').attrib['value'] = Req
+		req = root.find('config/REQUEST_PATH').attrib['value']
+		root.find('config/REQUEST_PATH').attrib['value'] = self.__dst(req,Req)
 		root.find('config/PARTER_ID').attrib['value'] = parter_id
 		info = root.find('config/PHP').attrib['infoPath']
-		root.find('config/PHP').attrib['infoPath'] = dst(info)
+		root.find('config/PHP').attrib['infoPath'] = self.__dst(info,assist)
 		comm = root.find('config/COMMUNITY_FRIEND_LIST_PATH').attrib['value']
-		root.find('config/COMMUNITY_FRIEND_LIST_PATH').attrib['value'] = dst(comm)
+		root.find('config/COMMUNITY_FRIEND_LIST_PATH').attrib['value'] = self.__dst(comm,assist)
 		interface = root.find('config/COMMUNITY_INTERFACE').attrib['path']
-		root.find('config/COMMUNITY_INTERFACE').attrib['path'] = dst(interface)
+		root.find('config/COMMUNITY_INTERFACE').attrib['path'] = self.__dst(interface,assist)
 		tree.write(self.__Flash_config, encoding='utf-8', xml_declaration=True)
 
 	def Flash_web(self, login, logout, title):
@@ -103,9 +112,18 @@ class change:
 			for l in tmp:
 				wd.writelines(l)
 
+	def keyfile(self,key):
+		with open(self.__key,'w') as f:
+			f.write(key)
+
 def main():
 	event = change()
-
+	z_id = int(input('请输入中控ID：'))
+	z_name = input('请输入频道名：')
+	z_sname = input('请输入s开头的域名：')
+	z_quename = input('请输入quest开头的域名：')
+	z_assisname = input('请输入assist开头的域名：')
+	z_key = input('请输入启动游戏Key：')
 	while True:
 		print(
 			'''
@@ -117,6 +135,19 @@ def main():
 			hosts 文件
 			'''
 		)
-		p = int(input('请输入要修改的服务器[1:DB|2:iis]： '))
-		if p == 1:
-			pass
+		p = input('请输入要修改的服务器[0:quit|1:DB|2:iis]： ').strip()
+		if p == '1' or p.lower() == 'db':
+			print('修改Center.Service.exe.config文件')
+			event.DB_change(z_id,z_name)
+			print('修改key.txt文件')
+			event.keyfile(z_key)
+			print('DB文件修改完成，请检查')
+		if p.lower() == 'quit' or p == '0':
+			return
+		if p.lower() == 'iis' or 'p' == '2':
+			event.Road_config(z_name,z_id,z_name)
+			event.Flash_config(z_quename,z_id,z_assisname)
+			event.Flash_web(z_quename,z_sname,z_name)
+			#event.Flash_default()
+			event.Request_config(z_id,z_name)
+			event.host(z_quename)
