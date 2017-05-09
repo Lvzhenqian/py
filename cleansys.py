01d9ae9cadd25ff3962024edb45f90e1f3482f4b
Lv.zhenqian <276829290@qq.com>
2017-05-09 21:49:19 +0800

    abc

--- cleansys.py
+++ cleansys.py
@@ -1,59 +1,58 @@
 from hashlib import md5
 from shutil import move as mv
-import os, sys
-import threading
+import os, threading
 from queue import Queue
+
 
 class dispatcher:
 	def __init__(self, sourcefile):
 		self.q = Queue(maxsize=1000)
 		self.source = self.md5sum(sourcefile)
-		self.data = None
-		self.event = threading.Event()
-		self.cond = threading.Condition()
 
 	def md5sum(self, file: str) -> str:
 		try:
-			with open(file, 'rb') as f:
-				return md5(f.read()).hexdigest()
+			with open(file, 'rb') as fd:
+				return md5(fd.read()).hexdigest()
 		except FileNotFoundError:
 			return ''
 
-	def Do_delete(self, source, destion: list):
+	def Do_delete(self, dst):
 		tmp = r'D:\temp'
 		if not os.path.exists(tmp):
 			os.mkdir(tmp)
-		for d in destion:
-			if self.md5sum(d) == source:
-				mv(d, tmp)
-				print('move {} successful'.format(d))
-		return True
+		if self.md5sum(dst) == self.source:
+			mv(dst, tmp)
+			print('move {} successful'.format(dst))
+		return
 
-	def read_date(self, path):
-		for enter in  os.scandir(path):
+	def ds(self, xpath):
+		for enter in os.scandir(xpath):
 			if enter.is_file():
-				self.q.put(enter.path)
+				yield enter.path
 			else:
-				self.read_date(enter.path)
+				self.ds(enter.path)
 
-	def producer(self):
-		with self.cond:
-			self.read_date(r'C:\Windows\System32\drivers')
-			self.cond.notifyAll()
-		self.event.wait()
-		self.event.set()
+	def put(self, path):
+		print('start,producer')
 
-	def consumer(self):
-		while not self.event.is_set():
-			with self.cond:
-				self.cond.wait()
-				self.Do_delete(self.source, self.q.get())
+		g = self.ds(path)
+		while True:
+			try:
+				data = next(g)
+				self.q.put(data)
+			except StopIteration:
+				break
+
+	def get(self):
+		print('start get')
+		d = self.q.get(1)
+		self.Do_delete(d)
+		print('sss')
 
 
 if __name__ == '__main__':
-	condition = dispatcher(sys.argv[1])
-	master = threading.Thread(target=condition.producer, name='producer')
-	for x in range(20):
-		threading.Thread(target=condition.consumer, name='consumer').start()
-	master.start()
-	sys.exit()
+	condition = dispatcher(r'e:\adpcqsppehzolpenarg.sys')
+	q = threading.Thread(target=condition.put, args=(r'C:\Windows\System32\drivers',), name='put')
+	g = threading.Thread(target=condition.get, name='consumer')
+	q.start()
+	g.start()
